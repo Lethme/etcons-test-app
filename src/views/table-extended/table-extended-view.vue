927c6125ccfec57aee7b42b3f98256a212455814
Ashe <lethme@ya.ru>
2024-08-27 18:19:21 +0300

    added some more mock data to extended table task

--- src/views/table-extended/table-extended-view.vue
+++ src/views/table-extended/table-extended-view.vue
@@ -19,6 +19,7 @@ enum CategoryType {
     Accessories = "Accessories",
     Fitness = "Fitness",
     Clothing = "Clothing",
+    Electronics = "Electronics",
 }
 
 const columns = ref<Array<Column>>([
@@ -36,7 +37,12 @@ const data = ref([
     { code: "zz21cz3c1", name: "Blue Band", category: CategoryType.Fitness, quantity: 2 },
     { code: "244wgerg2", name: "Blue T-Shirt", category: CategoryType.Clothing, quantity: 25 },
     { code: "h456wer53", name: "Bracelet", category: CategoryType.Accessories, quantity: 73 },
-])
+    { code: "x9d2fgh7k", name: "Brown Purse", category: CategoryType.Accessories, quantity: 15 },
+    { code: "l3p9tuv6r", name: "Chakra Bracelet", category: CategoryType.Accessories, quantity: 100 },
+    { code: "n7m3bcd9x", name: "Galaxy Earrings", category: CategoryType.Accessories, quantity: 57 },
+    { code: "a5v4nmk8t", name: "Game Controller", category: CategoryType.Electronics, quantity: 22 },
+    { code: "k4n9bgt1v", name: "Gaming Set", category: CategoryType.Electronics, quantity: 4 },
+]);
 </script>
 
 <style lang="less">
