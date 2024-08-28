0b4fe98d3fd2d9b3e12fc6cca48564f288ac9149
Ashe <lethme@ya.ru>
2024-08-28 08:22:19 +0300

    added accept header to fetchPageTitle function request

--- src/utils/fetch-page-title/fetch-page-title.util.ts
+++ src/utils/fetch-page-title/fetch-page-title.util.ts
@@ -6,9 +6,12 @@ export const fetchPageTitle = async (url: string): Promise<string | undefined> =
         try {
             const { data } = await axios.get<Document>(url, {
                 responseType: 'document',
+                headers: {
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                }
             });
 
-            return data.title;
+            return data.title || undefined;
         } catch (exception) {
             console.error(exception);
 
